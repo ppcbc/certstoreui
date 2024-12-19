3f9243129ecb97257793bd7c5898f1374aefa9da
makis <mantzinos@hotmail.com>
2024-12-20 00:27:32 +0200

    use login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Register from "./components/Register";
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,49 @@
+import { useState } from "react";
 import React from "react";
 import "../App.css";
+import axios from "axios";
+import http from "../data/http";
 
 function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  function getUser(e) {
+    setUser(prev => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value
+      };
+    });
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      var response = await axios.post(http + "login", user);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div className="wrapper fadeInDown">
       <div id="formContent">
         <div class="fadeIn first">
           <h2>Login</h2>
         </div>
-        <form action="">
+        <form action="" onSubmit={onSubmit}>
           <input
             type="text"
             id="login"
             className="fadeIn second"
             name="email"
             placeholder="email"
+            value={user.email}
+            onChange={getUser}
           />
           <input
             type="text"
@@ -22,6 +51,8 @@ function Login() {
             class="fadeIn third"
             name="password"
             placeholder="password"
+            value={user.password}
+            onChange={getUser}
           />
           <input type="submit" class="fadeIn fourth" value="Log In"></input>
         </form>
