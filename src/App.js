40b1832d414c1fe2222674fe22a4de2373a0cab4
makis <mantzinos@hotmail.com>
2024-12-19 14:41:38 +0200

    install axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-                 Learn React
+        Learn React
+        <button className="btn btn-primary">primary</button>
       </header>
     </div>
   );
