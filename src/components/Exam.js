9b37a917631db27f9cd2a5177f6cc38911595aa2
Nikos-efstratiou <nickefstratiou@gmail.com>
2024-12-23 19:04:04 +0200

    grid

---
+++ src/components/Exam.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../css/Exam.css";
+
+function Exam() {
+  return (
+    <div className="container">
+      <div className="title">
+        <h1>hello</h1>
+      </div>
+      <div className="grid-container">
+        <div className="box box1">Box 1</div>
+        <div className="box box2">Box 2</div>
+        <div className="box box3">Box 3</div>
+        <div className="box box4">Box 4</div>
+        <div className="box box5">Box 5</div>
+      </div>
+    </div>
+  );
+}
+
+export default Exam;
