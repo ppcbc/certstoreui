08f3d0411da567b78a0eba49ffba858066c27bfc
ChristinaGrln <christinagkarlaouni@gmail.com>
2025-01-02 22:16:56 +0200

    About.css-About.js Home.js-Home.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -43,15 +43,24 @@ export default function Home() {
       <div className="my-home">
         <h1>Welcome to the Certification Shop</h1>
         <hr className="divider" />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <p className="main-text">
+        Welcome to the Certification Shop, your go-to platform for professional certifications in programming. 
+        Founded in 2024, our mission is to provide individuals with the tools they need to advance in the 
+        tech industry by offering high-quality, industry-recognized certifications in programming languages 
+        like C#, JavaScript, Python, and more.
+        Whether you're just starting your programming journey or you're a seasoned developer looking to enhance your skill set,
+        Certification Shop offers a wide range of exams designed to validate your expertise and help you stand out 
+        in the competitive tech landscape.
+        Join thousands of learners who trust Certification Shop for their professional development.
+        Start your certification journey with us today and unlock new opportunities!
+       <p className="question-certify">
+          <p>Ready to Get Certified?</p>
+          <p>Browse our certification programs today and take the next 
+            step in your journey to excellence.
+          </p>
         </p>
+        </p>
+       
 
         <div className="preview-certifications">
           <h1>Available Certifications</h1>
