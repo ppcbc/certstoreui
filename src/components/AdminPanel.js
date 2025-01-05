711f23cc6731d20008fc14a8e638bef415523722
makis <mantzinos@hotmail.com>
2025-01-05 06:14:07 +0200

    fdsf

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -14,105 +14,136 @@ export default function AdminPanel() {
       <div className="admin-panel">
         <h1>Admin Panel</h1>
         <ul>
-          <AdminPanelLi
-            title={"Create Question"}
-            description={
-              "Create a new question by filling in the required fields and options."
-            }
-            link={"/addexam"}
-          />
+          <div className="admin-panel-container">
+            <h2>Question</h2>
+            <div className="admin-panel-group">
+              <AdminPanelLi
+                title={"Create Question"}
+                description={
+                  "Create a new question by filling in the required fields and options."
+                }
+                link={"/addexam"}
+              />
 
-          <AdminPanelLi
-            title={"Update Question"}
-            description={
-              "Edit an existing question to make changes or corrections."
-            }
-            link={"/update-exam"}
-          />
+              <AdminPanelLi
+                title={"Update Question"}
+                description={
+                  "Edit an existing question to make changes or corrections."
+                }
+                link={"/update-exam"}
+              />
 
-          <AdminPanelLi
-            title={"Delete Question"}
-            description={"Remove an existing question from the system."}
-            link={"/delete-exam"}
-          />
+              <AdminPanelLi
+                title={"Delete Question"}
+                description={"Remove an existing question from the system."}
+                link={"/delete-exam"}
+              />
+            </div>
 
-          <AdminPanelLi
-            title={"Create Category"}
-            description={"Create a new category to organize questions."}
-            link={"/addexam-category"}
-          />
+            <div className="admin-panel-container">
+              <h2>Category</h2>
+              <div className="admin-panel-group">
+                <AdminPanelLi
+                  title={"Create Category"}
+                  description={"Create a new category to organize questions."}
+                  link={"/addexam-category"}
+                />
 
-          <AdminPanelLi
-            title={"Update Category"}
-            description={"Modify an existing category to update its details."}
-            link={"/updateexam-category"}
-          />
+                <AdminPanelLi
+                  title={"Update Category"}
+                  description={
+                    "Modify an existing category to update its details."
+                  }
+                  link={"/updateexam-category"}
+                />
 
-          <AdminPanelLi
-            title={"Delete Category"}
-            description={"Remove a category that is no longer needed."}
-            link={"/delete-exam-category"}
-          />
+                <AdminPanelLi
+                  title={"Delete Category"}
+                  description={"Remove a category that is no longer needed."}
+                  link={"/delete-exam-category"}
+                />
+              </div>
+            </div>
+            <div className="admin-panel-container">
+              <h2>Module</h2>
+              <div className="admin-panel-group">
+                <AdminPanelLi
+                  title={"Create Module"}
+                  description={
+                    "Add a new module category to structure content effectively."
+                  }
+                  link={"/addfull-category"}
+                />
 
-          <AdminPanelLi
-            title={"Create Module"}
-            description={
-              "Add a new module category to structure content effectively."
-            }
-            link={"/addfull-category"}
-          />
+                <AdminPanelLi
+                  title={"Update Module"}
+                  description={
+                    "Edit the details of an existing module category."
+                  }
+                  link={"/update-full-exam-category"}
+                />
 
-          <AdminPanelLi
-            title={"Update Module"}
-            description={"Edit the details of an existing module category."}
-            link={"/update-full-exam-category"}
-          />
+                <AdminPanelLi
+                  title={"Delete Module"}
+                  description={"Delete a module category from the system."}
+                  link={"/delete-full-exam-category"}
+                />
+              </div>
+            </div>
+            <div className="admin-panel-container">
+              <h2>Exam</h2>
+              <div className="admin-panel-group">
+                <AdminPanelLi
+                  title={"Create Exam"}
+                  description={"Design and add a new exam test to the system."}
+                  link={"/add-certexam"}
+                />
 
-          <AdminPanelLi
-            title={"Delete Module"}
-            description={"Delete a module category from the system."}
-            link={"/delete-full-exam-category"}
-          />
+                <AdminPanelLi
+                  title={"Delete Exam"}
+                  description={"Remove an existing exam test permanently."}
+                  link={"/delete-certexam"}
+                />
+              </div>
+            </div>
+            <div className="admin-panel-container">
+              <h2>User</h2>
+              <div className="admin-panel-group">
+                <AdminPanelLi
+                  title={"Update User"}
+                  description={
+                    "Modify user details such as name, email, or other attributes."
+                  }
+                  link={"/update-user"}
+                />
 
-          <AdminPanelLi
-            title={"Create Exam"}
-            description={"Design and add a new exam test to the system."}
-            link={"/add-certexam"}
-          />
-
-          <AdminPanelLi
-            title={"Delete Exam"}
-            description={"Remove an existing exam test permanently."}
-            link={"/delete-certexam"}
-          />
-
-          <AdminPanelLi
-            title={"Update User"}
-            description={
-              "Modify user details such as name, email, or other attributes."
-            }
-            link={"/update-user"}
-          />
-
-          <AdminPanelLi
-            title={"Delete User"}
-            description={"Permanently remove a user from the system"}
-            link={"/delete-user"}
-          />
-          <AdminPanelLi
-            title={"Create Role"}
-            description={
-              "Select and assign a role to a user, defining their level of access."
-            }
-            link={"/create-role"}
-          />
-          <AdminPanelLi
-            title={"Role Access"}
-            description={
-              "Select and assign a role to a user, defining their level of access."
-            }
-            link={"/assignrole"}
-          />
+                <AdminPanelLi
+                  title={"Delete User"}
+                  description={"Permanently remove a user from the system"}
+                  link={"/delete-user"}
+                />
+              </div>
+            </div>
+            <div className="admin-panel-container">
+              <h2>Role</h2>
+              <div className="admin-panel-group">
+                <AdminPanelLi
+                  title={"Create Role"}
+                  description={
+                    "Select and assign a role to a user, defining their level of access."
+                  }
+                  link={"/create-role"}
+                />
+                <AdminPanelLi
+                  title={"Role Access"}
+                  description={
+                    "Select and assign a role to a user, defining their level of access."
+                  }
+                  link={"/assignrole"}
+                />
+              </div>
+            </div>
+          </div>
         </ul>
       </div>
       <Footer color={"lightgrey"} />
--- src/components/AdminPanelLi.js
+++ src/components/AdminPanelLi.js
@@ -4,15 +4,17 @@ import { useNavigate } from "react-router-dom";
 function AdminPanelLi({ title, description, link }) {
   const navigate = useNavigate();
   return (
-    <li>
-      <div className="admin-panel-item-header">
-        <h2>{title}</h2>
-        <div className="admin-panel-paragraph-container">
-          <p className="admin-panel-item-paragraph">{description}</p>
+    <div className="admin-panel-item">
+      <li>
+        <div className="admin-panel-item-header">
+          <h2>{title}</h2>
+          <div className="admin-panel-paragraph-container">
+            <p className="admin-panel-item-paragraph">{description}</p>
+          </div>
+          <button onClick={() => navigate(`${link}`)}>Go</button>
         </div>
-        <button onClick={() => navigate(`${link}`)}>Go</button>
-      </div>
-    </li>
+      </li>
+    </div>
   );
 }
 
